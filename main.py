b8d31bd424a684e24575d3c74384f0a95c4f18dc
Cobiscuit <cobainirwin@gmail.com>
2025-08-06 23:34:42 -0500

    .3 I added onto the main.py file to be able to make a game window and have it drawn an infinate amount of times.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from constants import *
+import pygame
+
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return play_game == False
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 
+            
+        screen.fill("black")
+        pygame.display.flip()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
